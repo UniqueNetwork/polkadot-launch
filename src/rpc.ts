d6273f4a326d4100efec449d5c7121a3366118d2
Yaroslav Bolyukin <iam@lach.pw>
2022-08-02 08:45:42 +0000

    refactor: cleanup transaction execution
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- src/rpc.ts
+++ src/rpc.ts
@@ -1,5 +1,6 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { Keyring } from "@polkadot/api";
+import { KeyringPair } from "@polkadot/keyring/types";
 import { cryptoWaitReady, blake2AsHex } from "@polkadot/util-crypto";
 import fs from "fs";
 
@@ -42,50 +43,19 @@ export async function registerParachain(
 	header: string,
 	finalization: boolean = false
 ) {
-	return new Promise<void>(async (resolvePromise, reject) => {
-		await cryptoWaitReady();
+	await cryptoWaitReady();
 
-		const keyring = new Keyring({ type: "sr25519" });
-		const alice = keyring.addFromUri("//Alice");
+	const alice = privateKey("//Alice");
 
-		let paraGenesisArgs = {
-			genesis_head: header,
-			validation_code: wasm,
-			parachain: true,
-		};
-		let genesis = api.createType("ParaGenesisArgs", paraGenesisArgs);
+	let paraGenesisArgs = {
+		genesis_head: header,
+		validation_code: wasm,
+		parachain: true,
+	};
+	let genesis = api.createType("ParaGenesisArgs", paraGenesisArgs);
 
-		const nonce = Number((await api.query.system.account(alice.address)).nonce);
-
-		console.log(
-			`--- Submitting extrinsic to register parachain ${id}. (nonce: ${nonce}) ---`
-		);
-		const unsub = await api.tx.sudo
-			.sudo(api.tx.parasSudoWrapper.sudoScheduleParaInitialize(id, genesis))
-			.signAndSend(alice, { nonce: nonce, era: 0 }, (result) => {
-				console.log(`Current status is ${result.status}`);
-				if (result.status.isInBlock) {
-					console.log(
-						`Transaction included at blockHash ${result.status.asInBlock}`
-					);
-					if (finalization) {
-						console.log("Waiting for finalization...");
-					} else {
-						unsub();
-						resolvePromise();
-					}
-				} else if (result.status.isFinalized) {
-					console.log(
-						`Transaction finalized at blockHash ${result.status.asFinalized}`
-					);
-					unsub();
-					resolvePromise();
-				} else if (result.isError) {
-					console.log(`Transaction Error`);
-					reject(`Transaction Error`);
-				}
-			});
-	});
+	console.log(`--- Submitting extrinsic to register parachain ${id}. ---`);
+	await executeTransaction(api, alice, api.tx.sudo.sudo(api.tx.parasSudoWrapper.sudoScheduleParaInitialize(id, genesis)), finalization);
 }
 
 // Set the balance of an account on the relay chain.
@@ -95,20 +65,28 @@ export async function setBalance(
 	value: string,
 	finalization: boolean = false
 ) {
+	await cryptoWaitReady();
+
+	const alice = privateKey('//Alice');
+
+	console.log(`--- Submitting extrinsic to set balance of ${who} to ${value}. ---`);
+	await executeTransaction(api, alice, api.tx.sudo.sudo(api.tx.balances.setBalance(who, value, 0)), finalization);
+}
+
+// Set the balance of an account on the relay chain.
+export async function executeTransaction(
+	api: ApiPromise,
+	sender: KeyringPair,
+	tx: any,
+	finalization: boolean = false
+) {
 	return new Promise<void>(async (resolvePromise, reject) => {
 		await cryptoWaitReady();
 
-		const keyring = new Keyring({ type: "sr25519" });
-		const alice = keyring.addFromUri("//Alice");
+		const nonce = Number((await api.query.system.account(sender.address)).nonce);
 
-		const nonce = Number((await api.query.system.account(alice.address)).nonce);
-
-		console.log(
-			`--- Submitting extrinsic to set balance of ${who} to ${value}. (nonce: ${nonce}) ---`
-		);
-		const unsub = await api.tx.sudo
-			.sudo(api.tx.balances.setBalance(who, value, 0))
-			.signAndSend(alice, { nonce: nonce, era: 0 }, (result) => {
+		const unsub = await tx
+			.signAndSend(sender, { nonce: nonce, era: 0 }, (result: any) => {
 				console.log(`Current status is ${result.status}`);
 				if (result.status.isInBlock) {
 					console.log(
@@ -132,6 +110,12 @@ export async function setBalance(
 				}
 			});
 	});
+}
+
+export function privateKey(seed: string): KeyringPair {
+	const keyring = new Keyring({ type: "sr25519" });
+	const user = keyring.addFromUri(seed);
+	return user;
 }
 
 // Perform a forkless runtime upgrade on the relay
@@ -141,45 +125,15 @@ export async function upgradeRelayRuntime(
 	finalization: boolean = false,
 	old_tag?: string,
 	new_tag?: string,
-) { 
-	return new Promise<void>(async (resolvePromise, reject) => {
-		await cryptoWaitReady();
+) {
+	await cryptoWaitReady();
 
-		const keyring = new Keyring({ type: "sr25519" });
-		const alice = keyring.addFromUri("//Alice");
+	const alice = privateKey("//Alice");
 
-		const nonce = Number((await api.query.system.account(alice.address)).nonce);
-		const code = fs.readFileSync(wasm).toString('hex');
+	const code = fs.readFileSync(wasm).toString('hex');
 
-		console.log(
-			`--- Upgrading the relay chain runtime from ${old_tag ? old_tag : wasm} ${new_tag ? `to ${new_tag}` : ""}. (nonce: ${nonce}) ---`
-		);
-		const unsub = await api.tx.sudo
-			.sudoUncheckedWeight(api.tx.system.setCode(`0x${code}`), 0)
-			.signAndSend(alice, { nonce: nonce, era: 0 }, (result) => {
-				console.log(`Current status is ${result.status}`);
-				if (result.status.isInBlock) {
-					console.log(
-						`Transaction included at blockHash ${result.status.asInBlock}`
-					);
-					if (finalization) {
-						console.log("Waiting for finalization...");
-					} else {
-						unsub();
-						resolvePromise();
-					}
-				} else if (result.status.isFinalized) {
-					console.log(
-						`Transaction finalized at blockHash ${result.status.asFinalized}`
-					);
-					unsub();
-					resolvePromise();
-				} else if (result.isError) {
-					console.log(`Transaction Error`);
-					reject(`Transaction Error`);
-				}
-			});
-	});
+	console.log(`--- Upgrading the relay chain runtime from ${old_tag ? old_tag : wasm} ${new_tag ? `to ${new_tag}` : ""}. ---`);
+	await executeTransaction(api, alice, api.tx.sudo.sudoUncheckedWeight(api.tx.system.setCode(`0x${code}`), 0));
 }
 
 // Perform a forkless runtime upgrade on a parachain
@@ -189,85 +143,21 @@ export async function upgradeParachainRuntime(
 	finalization: boolean = true,
 	old_tag?: string,
 	new_tag?: string,
-) { 
+) {
 	const code = fs.readFileSync(wasm);
 	const codeHash = blake2AsHex(code); // 256
 
-	await new Promise<void>(async (resolvePromise, reject) => {
-		await cryptoWaitReady();
+	await cryptoWaitReady();
 
-		const keyring = new Keyring({ type: "sr25519" });
-		const alice = keyring.addFromUri("//Alice");
+	const alice = privateKey("//Alice");
 
-		const nonce = Number((await api.query.system.account(alice.address)).nonce);
+	console.log(`--- Authorizing the parachain runtime upgrade from ${old_tag ? old_tag : wasm} ${new_tag ? `to ${new_tag}` : ""}. ---`);
+	await executeTransaction(api, alice, api.tx.sudo
+		.sudoUncheckedWeight(api.tx.parachainSystem.authorizeUpgrade(codeHash), 0), finalization);
 
-		console.log(
-			`--- Authorizing the parachain runtime upgrade from ${old_tag ? old_tag : wasm} ${new_tag ? `to ${new_tag}` : ""}. (nonce: ${nonce}) ---`
-		);
-		const unsub = await api.tx.sudo
-			.sudoUncheckedWeight(api.tx.parachainSystem.authorizeUpgrade(codeHash), 0)
-			.signAndSend(alice, { nonce: nonce, era: 0 }, (result) => {
-				console.log(`Current status is ${result.status}`);
-				if (result.status.isInBlock) {
-					console.log(
-						`Transaction included at blockHash ${result.status.asInBlock}`
-					);
-					if (finalization) {
-						console.log("Waiting for finalization...");
-					} else {
-						unsub();
-						resolvePromise();
-					}
-				} else if (result.status.isFinalized) {
-					console.log(
-						`Transaction finalized at blockHash ${result.status.asFinalized}`
-					);
-					unsub();
-					resolvePromise();
-				} else if (result.isError) {
-					console.log(`Transaction Error`);
-					reject(`Transaction Error`);
-				}
-			});
-	});
-
-	return new Promise<void>(async (resolvePromise, reject) => {
-		await cryptoWaitReady();
-
-		const keyring = new Keyring({ type: "sr25519" });
-		const alice = keyring.addFromUri("//Alice");
-
-		const nonce = Number((await api.query.system.account(alice.address)).nonce);
-
-		console.log(
-			`--- Upgrading the parachain runtime. (nonce: ${nonce}) ---`
-		);
-		const unsub = await api.tx.sudo
-		.sudoUncheckedWeight(api.tx.parachainSystem.enactAuthorizedUpgrade(`0x${code.toString('hex')}`), 0)
-		.signAndSend(alice, { nonce: nonce, era: 0 }, (result) => {
-			console.log(`Current status is ${result.status}`);
-			if (result.status.isInBlock) {
-				console.log(
-					`Transaction included at blockHash ${result.status.asInBlock}`
-				);
-				if (finalization) {
-					console.log("Waiting for finalization...");
-				} else {
-					unsub();
-					resolvePromise();
-				}
-			} else if (result.status.isFinalized) {
-				console.log(
-					`Transaction finalized at blockHash ${result.status.asFinalized}`
-				);
-				unsub();
-				resolvePromise();
-			} else if (result.isError) {
-				console.log(`Transaction Error`);
-				reject(`Transaction Error`);
-			}
-		});
-	});
+	console.log(`--- Upgrading the parachain runtime. ---`);
+	await executeTransaction(api, alice, api.tx.sudo
+		.sudoUncheckedWeight(api.tx.parachainSystem.enactAuthorizedUpgrade(`0x${code.toString('hex')}`), 0));
 }
 
 export interface RelayInfo {
@@ -278,40 +168,33 @@ export interface RelayInfo {
 
 export async function getSpecVersion(
 	api: ApiPromise,
-): Promise<number> { 
-	return new Promise(async (resolve) => {
-		const info = api.consts.system.version.specVersion.toNumber();
-  		resolve(info);
-	});
+): Promise<number> {
+	return api.consts.system.version.specVersion.toNumber();
 }
 
 export async function getRelayInfo(
 	api: ApiPromise,
-): Promise<RelayInfo> { 
-	return new Promise(async (resolve) => {
-		const info = {
-			specVersion: api.consts.system.version.specVersion.toNumber(),
-			epochLength: api.consts.babe.epochDuration.toNumber(),
-			blockTime: api.consts.babe.expectedBlockTime.toNumber(),
-		};
-  		resolve(info);
-	});
+): Promise<RelayInfo> {
+	const info = {
+		specVersion: api.consts.system.version.specVersion.toNumber(),
+		epochLength: api.consts.babe.epochDuration.toNumber(),
+		blockTime: api.consts.babe.expectedBlockTime.toNumber(),
+	};
+	return info;
 }
 
 export async function getCodeValidationDelay(
 	api: ApiPromise,
-): Promise<number> { 
-	return new Promise(async (resolve) => {
-		const { validationUpgradeDelay, minimumValidationUpgradeDelay } = (await api.query.configuration.activeConfig()).toJSON() as any;
-		let delay = 0;
-		if (validationUpgradeDelay !== undefined) {
-			delay = validationUpgradeDelay;
-			if (minimumValidationUpgradeDelay !== undefined) {
-				delay = Math.max(delay, minimumValidationUpgradeDelay);
-			}
+): Promise<number> {
+	const { validationUpgradeDelay, minimumValidationUpgradeDelay } = (await api.query.configuration.activeConfig()).toJSON() as any;
+	let delay = 0;
+	if (validationUpgradeDelay !== undefined) {
+		delay = validationUpgradeDelay;
+		if (minimumValidationUpgradeDelay !== undefined) {
+			delay = Math.max(delay, minimumValidationUpgradeDelay);
 		}
-  		resolve(delay);
-	});
+	}
+	return delay;
 }
 
 export async function sendHrmpMessage(
@@ -320,45 +203,17 @@ export async function sendHrmpMessage(
 	data: string,
 	finalization: boolean = false
 ) {
-	return new Promise<void>(async (resolvePromise, reject) => {
-		await cryptoWaitReady();
+	await cryptoWaitReady();
 
-		const keyring = new Keyring({ type: "sr25519" });
-		const alice = keyring.addFromUri("//Alice");
+	const alice = privateKey("//Alice");
 
-		const nonce = Number((await api.query.system.account(alice.address)).nonce);
+	let hrmpMessage = {
+		recipient: recipient,
+		data: data,
+	};
+	let message = api.createType("OutboundHrmpMessage", hrmpMessage);
 
-		let hrmpMessage = {
-			recipient: recipient,
-			data: data,
-		};
-		let message = api.createType("OutboundHrmpMessage", hrmpMessage);
-
-		console.log(`--- Sending a message to ${recipient}. (nonce: ${nonce}) ---`);
-		const unsub = await api.tx.sudo
-			.sudo(api.tx.messageBroker.sudoSendHrmpMessage(message))
-			.signAndSend(alice, { nonce: nonce, era: 0 }, (result) => {
-				console.log(`Current status is ${result.status}`);
-				if (result.status.isInBlock) {
-					console.log(
-						`Transaction included at blockHash ${result.status.asInBlock}`
-					);
-					if (finalization) {
-						console.log("Waiting for finalization...");
-					} else {
-						unsub();
-						resolvePromise();
-					}
-				} else if (result.status.isFinalized) {
-					console.log(
-						`Transaction finalized at blockHash ${result.status.asFinalized}`
-					);
-					unsub();
-					resolvePromise();
-				} else if (result.isError) {
-					console.log(`Transaction Error`);
-					reject(`Transaction Error`);
-				}
-			});
-	});
+	console.log(`--- Sending a message to ${recipient}. ---`);
+	await executeTransaction(api, alice, api.tx.sudo
+		.sudo(api.tx.messageBroker.sudoSendHrmpMessage(message)), finalization);
 }
