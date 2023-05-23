503e0dd2e39eb61e0f3423fbebf9d92ecef0e890
Max Andreev <maksandre@yandex.ru>
2023-05-23 09:20:18 +0000

    Use forceSetBalance if exist

--- src/rpc.ts
+++ src/rpc.ts
@@ -69,7 +69,10 @@ export async function setBalance(
 	const superuser = privateKey('//Alice');
 
 	console.log(`--- Submitting extrinsic to set balance of ${who} to ${value}. ---`);
-	await executeTransaction(api, superuser, api.tx.sudo.sudo(api.tx.balances.setBalance(who, value, 0)), finalization);
+	const setBalanceTx = api.tx.balances.forceSetBalance 
+		? api.tx.balances.forceSetBalance(who, value, 0)
+		: api.tx.balances.setBalance(who, value, 0);
+	await executeTransaction(api, superuser, api.tx.sudo.sudo(setBalanceTx), finalization);
 }
 
 // Set the balance of an account on the relay chain.
